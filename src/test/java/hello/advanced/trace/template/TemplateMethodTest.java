cc247f809fe746a1c8f0c29f027ef0c6aacdffdc
shkim <skok1025@naver.com>
2022-11-09 11:40:53 +0900

    전략패턴 학습중

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -63,4 +63,26 @@ public class TemplateMethodTest {
         template2.execute();
 
     }
+
+    @Test
+    void templateMethodV3() {
+        AbstractTemplate template1 =  new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+
+        template1.execute();
+
+        AbstractTemplate template2 =  new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+
+        template2.execute();
+
+    }
 }
