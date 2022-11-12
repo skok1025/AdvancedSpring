4d7b261b66d04a1fa16ee47aea39553878aa078d
shkim <skok1025@naver.com>
2022-11-12 14:16:33 +0900

    탬플릿 콜백 패턴 - 너무 어렵네요...

---
+++ src/test/java/hello/advanced/trace/strategy/code/template/TimeLogTemplate.java
@@ -0,0 +1,15 @@
+package hello.advanced.trace.strategy.code.template;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TimeLogTemplate {
+    public void execute(Callback callback) {
+        long startTime = System.currentTimeMillis();
+        callback.call();
+        long endTime = System.currentTimeMillis();
+
+        long resultTIme = endTime - startTime;
+        log.info("resultTime={}", resultTIme);
+    }
+}
